737683179cfaff5a2486854a9188e917df754832
jeonghun.kim <jeonghun.kim@monolith.co.kr>
2026-03-15 15:35:43 +0900

    feat: Update app icons and splash screens for all platforms and integrate `@capacitor/assets`.

--- vite.config.ts
+++ vite.config.ts
@@ -22,19 +22,19 @@ export default defineConfig({
         display: 'standalone',
         icons: [
           {
-            src: 'coffee.svg',
+            src: 'coffee-192.png',
             sizes: '192x192',
-            type: 'image/svg+xml'
+            type: 'image/png'
           },
           {
-            src: 'coffee.svg',
+            src: 'coffee-512.png',
             sizes: '512x512',
-            type: 'image/svg+xml'
+            type: 'image/png'
           },
           {
-            src: 'coffee.svg',
+            src: 'coffee-512.png',
             sizes: 'any',
-            type: 'image/svg+xml',
+            type: 'image/png',
             purpose: 'any maskable'
           }
         ]
